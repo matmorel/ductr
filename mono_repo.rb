855fde6d376fd382263ca6a1f4488ecd945fa68f
matmorel <matmorel@users.noreply.github.com>
2025-05-02 12:26:58 +0200

    setup monorepo

--- mono_repo.rb
+++ mono_repo.rb
@@ -5,6 +5,33 @@ module MonoRepo
   SEQUEL = ["sequel", "~> 5.91"].freeze
 
   class << self
+    def add_metadata(gemspec)
+      gemspec.authors = ["Mathieu Morel"]
+      gemspec.email = ["matmorel@users.noreply.github.com"]
+      gemspec.license = "LGPL-3.0-or-later"
+      gemspec.homepage = "https://github.com/matmorel/ductr"
+      # gemspec.metadata["homepage_uri"] = gemspec.homepage
+      gemspec.metadata["source_code_uri"] = gemspec.homepage
+      gemspec.metadata["changelog_uri"] = "#{gemspec.homepage}/releases"
+      gemspec.required_ruby_version = ">= 3.1.0"
+      gemspec.metadata["allowed_push_host"] = "https://rubygems.org"
+      gemspec.metadata["rubygems_mfa_required"] = "true"
+    end
+
+    def add_files(gemspec, current_dir, current_file)
+      # Specify which files should be added to the gem when it is released.
+      # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
+      gemspec.files = Dir.chdir(File.expand_path(current_dir)) do
+        `git ls-files -z`.split("\x0").reject do |f|
+          (f == current_file) || f.match(%r{\A(?:(?:bin|test|spec|features)/|\.(?:git|travis|circleci|vscode)|appveyor)})
+        end
+      end
+
+      gemspec.bindir = "exe"
+      gemspec.executables = gemspec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
+      gemspec.require_paths = ["lib"]
+    end
+
     def add_development_dependencies(gemspec)
       gemspec.add_development_dependency "debug", "~> 1.10"
       gemspec.add_development_dependency "rake", "~> 13.0"
--- sqlite/ductr-sqlite.gemspec
+++ sqlite/ductr-sqlite.gemspec
@@ -6,33 +6,11 @@ require_relative "lib/ductr/sqlite/version"
 Gem::Specification.new do |spec|
   spec.name = "ductr-sqlite"
   spec.version = Ductr::SQLite::VERSION
-  spec.authors = ["Mathieu MOREL"]
-  spec.email = ["matmorel@users.noreply.github.com"]
-  spec.licenses = ["LGPL-3.0-or-later"]
-
   spec.summary = "SQLite adapter for Ductr using the `sequel` gem"
   spec.description = "Allows ductr to interact with SQLite DMBS."
-  spec.homepage = "https://github.com/matmorel/ductr"
-  spec.required_ruby_version = ">= 3.1.0"
 
-  spec.metadata["allowed_push_host"] = "https://rubygems.org"
-  spec.metadata["rubygems_mfa_required"] = "true"
-
-  spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = spec.homepage
-  spec.metadata["changelog_uri"] = "#{spec.homepage}/releases"
-
-  # Specify which files should be added to the gem when it is released.
-  # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files = Dir.chdir(File.expand_path(__dir__)) do
-    `git ls-files -z`.split("\x0").reject do |f|
-      (f == __FILE__) || f.match(%r{\A(?:(?:bin|test|spec|features)/|\.(?:git|travis|circleci)|appveyor)})
-    end
-  end
-  spec.bindir = "exe"
-  spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
-
+  MonoRepo.add_metadata(spec)
+  MonoRepo.add_files(spec, __dir__, __FILE__)
   MonoRepo.add_development_dependencies(spec)
 
   spec.add_dependency(*MonoRepo::DUCTR)
